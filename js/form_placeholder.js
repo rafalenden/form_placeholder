f4a890ac5d4f3a4a9e8d32c9596b4043fbbf8f5a
rafal.enden <rafal@enden.pl>
2013-05-06 13:39:27 +0200

    Code cleanups

--- js/form_placeholder.js
+++ js/form_placeholder.js
@@ -1,43 +1,37 @@
 (function($, Drupal) {
 
-  function form_placeholder_attach(context) {
-
-    var include = Drupal.settings.form_placeholder.include;
-    var exclude = Drupal.settings.form_placeholder.exclude;
-    var required = Drupal.settings.form_placeholder.required;
-
-    $(include, context).not(exclude).each(function() {
-
-      textfield = $(this);
-
-      // Check if element is a textfield
-      if (!textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
-        return;
-      }
-
-      form = textfield.closest('form');
-      label = form.find('label[for=' + textfield.attr('id') + ']');
-
-      if (required == 'append') {
-        label.find('.form-required').insertAfter(textfield).prepend('&nbsp;');
-      }
-      else if (required == 'remove') {
-        label.find('.form-required').remove();
-      }
-      else if (required == 'text') {
-        label.find('.form-required').text('(' + Drupal.t('required') + ')');
-      }
-
-      label.hide();
-
-      textfield.attr('placeholder', label.text());
-      textfield.placeholder();
-    });
-  }
-
   Drupal.behaviors.form_placeholder = {
-    attach: function(context) {
-      form_placeholder_attach(context);
+    attach: function(context, settings) {
+      var include = Drupal.settings.form_placeholder.include;
+      var exclude = Drupal.settings.form_placeholder.exclude;
+      var required = Drupal.settings.form_placeholder.required;
+
+      $(include, context).not(exclude).each(function() {
+        textfield = $(this);
+
+        // Check if element is a textfield
+        if (!textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
+          return;
+        }
+
+        form = textfield.closest('form');
+        label = form.find('label[for=' + textfield.attr('id') + ']');
+
+        if (required == 'append') {
+          label.find('.form-required').insertAfter(textfield).prepend('&nbsp;');
+        }
+        else if (required == 'remove') {
+          label.find('.form-required').remove();
+        }
+        else if (required == 'text') {
+          label.find('.form-required').text('(' + Drupal.t('required') + ')');
+        }
+
+        label.hide();
+
+        textfield.attr('placeholder', label.text());
+        textfield.placeholder();
+      });
     }
   }
 
