6ada02f33d07735239ded34ed0f1b40d42fbf385
rafal.enden <rafal@enden.pl>
2013-08-04 19:06:21 +0200

    Fixed coding standards issues.

--- form_placeholder.js
+++ form_placeholder.js
@@ -1,4 +1,4 @@
-(function($, Drupal) {
+(function($) {
 
   Drupal.behaviors.form_placeholder = {
     attach: function(context, settings) {
@@ -13,35 +13,35 @@
       var required_indicator = Drupal.settings.form_placeholder.required_indicator;
 
       $(include, context).not(exclude).each(function() {
-        textfield = $(this);
+        $textfield = $(this);
 
         // Check if element is a textfield
-        if (!textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
+        if (!$textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
           return;
         }
 
-        form = textfield.closest('form');
-        label = form.find('label[for=' + this.id + ']');
+        $form = $textfield.closest('form');
+        $label = $form.find('label[for=' + this.id + ']');
 
         if (required_indicator === 'append') {
-          label.find('.form-required').insertAfter(textfield).prepend('&nbsp;');
+          $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
         }
         else if (required_indicator === 'remove') {
-          label.find('.form-required').remove();
+          $label.find('.form-required').remove();
         }
         else if (required_indicator === 'text') {
-          label.find('.form-required').text('(' + Drupal.t('required') + ')');
+          $label.find('.form-required').text('(' + Drupal.t('required') + ')');
         }
 
-        label.hide();
-        textfield.attr('placeholder', label.text());
+        $label.hide();
+        $textfield.attr('placeholder', $label.text());
 
         // If the jQuery Placeholder plugin is loaded correctly.
         if (Drupal.settings.form_placeholder.fallback_support) {
-          textfield.placeholder();
+          $textfield.placeholder();
         }
       });
     }
   }
 
-})(jQuery, Drupal);
+})(jQuery);
