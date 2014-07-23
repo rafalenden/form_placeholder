60b66c59300e390204c52640802092238c414324
Rafal Enden <rafal.enden@1576260.no-reply.drupal.org>
2014-07-24 00:26:58 +0100

    Issue #2306177 by CheeseDurger: Label and placeholder both hidden in IE 7-9.

--- form_placeholder.js
+++ form_placeholder.js
@@ -1,56 +1,62 @@
 (function($) {
 
+  Drupal.form_placeholder = {};
+
+  Drupal.form_placeholder.placeholderIsSupported = function() {
+    // Opera Mini v7 doesn’t support placeholder although its DOM seems to indicate so.
+    var isOperaMini = Object.prototype.toString.call(window.operamini) == '[object OperaMini]';
+    return 'placeholder' in document.createElement('input') && !isOperaMini;
+  };
+
   Drupal.behaviors.form_placeholder = {
     attach: function(context, settings) {
-      var include = Drupal.settings.form_placeholder.include;
+      var include = settings.form_placeholder.include;
       if (include) {
         include += ', ';
       }
       include += '.form-placeholder-include-children *';
       include += ', .form-placeholder-include';
-      var exclude = Drupal.settings.form_placeholder.exclude;
+      var exclude = settings.form_placeholder.exclude;
       if (exclude) {
         exclude += ', ';
       }
       exclude += '.form-placeholder-exclude-children *';
       exclude += ', .form-placeholder-exclude';
 
-      var required_indicator = Drupal.settings.form_placeholder.required_indicator;
+      var required_indicator = settings.form_placeholder.required_indicator;
 
       $(include, context).not(exclude).each(function() {
         $textfield = $(this);
 
-        // Check if element is a textfield
+        // Check if element is a textfield.
         if (!$textfield.is('input[type=text], input[type=email], input[type=password], textarea')) {
           return;
         }
+        // Placeholder is supported.
+        else if (Drupal.form_placeholder.placeholderIsSupported() || settings.form_placeholder.fallback_support) {
+          $form = $textfield.closest('form');
+          $label = $form.find('label[for=' + this.id + ']');
 
-        $form = $textfield.closest('form');
-        $label = $form.find('label[for=' + this.id + ']');
+          if (required_indicator === 'append') {
+            $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
+          }
+          else if (required_indicator === 'remove') {
+            $label.find('.form-required').remove();
+          }
+          else if (required_indicator === 'text') {
+            $label.find('.form-required').text('(' + Drupal.t('required') + ')');
+          }
 
-        if (required_indicator === 'append') {
-          $label.find('.form-required').insertAfter($textfield).prepend('&nbsp;');
-        }
-        else if (required_indicator === 'remove') {
-          $label.find('.form-required').remove();
-        }
-        else if (required_indicator === 'text') {
-          $label.find('.form-required').text('(' + Drupal.t('required') + ')');
-        }
-
-        $textfield.attr('placeholder', $.trim($label.text()));
-
-        // If the jQuery Placeholder plugin is loaded correctly.
-        if (Drupal.settings.form_placeholder.fallback_support) {
+          $textfield.attr('placeholder', $.trim($label.text()));
           $label.hide();
-          $textfield.placeholder();
-        }
-        // If user browser is not < IE 10.
-        else if ($textfield.attr('placeholder') !== undefined && $textfield.attr('placeholder') !== '') {
-          $label.hide();
+
+          // Fallback support for older browsers.
+          if (!Drupal.form_placeholder.placeholderIsSupported() && settings.form_placeholder.fallback_support) {
+            $textfield.placeholder();
+          }
         }
       });
     }
-  }
+  };
 
 })(jQuery);
